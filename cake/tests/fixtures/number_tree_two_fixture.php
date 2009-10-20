ff5d6c3b15493dae6de7c0bc56846b9949aba2e0
predominant <graham@grahamweldon.com>
2009-10-20 14:18:13 +1100

    Adding missing fixture.

---
+++ cake/tests/fixtures/number_tree_two_fixture.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Tree behavior class.
+ *
+ * Enables a model object to act as a node-based tree.
+ *
+ * PHP Version 5.x
+ *
+ * CakePHP(tm) Tests <https://trac.cakephp.org/wiki/Developement/TestSuite>
+ * Copyright 2005-2009, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
+ *
+ * Licensed under The Open Group Test Suite License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @filesource
+ * @copyright     Copyright 2005-2009, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
+ * @link          https://trac.cakephp.org/wiki/Developement/TestSuite CakePHP(tm) Tests
+ * @package       cake
+ * @subpackage    cake.tests.fixtures
+ * @since         CakePHP(tm) v 1.2.0.5331
+ * @license       http://www.opensource.org/licenses/opengroup.php The Open Group Test Suite License
+ */
+
+/**
+ * Number Tree Test Fixture
+ *
+ * Generates a tree of data for use testing the tree behavior
+ *
+ * @package       cake
+ * @subpackage    cake.tests.fixtures
+ */
+class NumberTreeTwoFixture extends CakeTestFixture {
+
+/**
+ * name property
+ *
+ * @var string 'NumberTree'
+ * @access public
+ */
+	var $name = 'NumberTreeTwo';
+
+/**
+ * fields property
+ *
+ * @var array
+ * @access public
+ */
+	var $fields = array(
+		'id'	=> array('type' => 'integer','key' => 'primary'),
+		'name'	=> array('type' => 'string','null' => false),
+		'number_tree_id' => array('type' => 'integer', 'null' => false),
+		'parent_id' => 'integer',
+		'lft'	=> array('type' => 'integer','null' => false),
+		'rght'	=> array('type' => 'integer','null' => false)
+	);
+}
+?>
